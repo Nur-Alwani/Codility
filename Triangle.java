3e742c4b9c7f15e24803a0634564a4748f244a25
Nur Alwani <Nur-Alwani@users.noreply.github.com>
2017-04-17 18:06:16 +0200

    Update Triangle.java

--- Triangle.java
+++ Triangle.java
@@ -1,15 +1,11 @@
 import java.util.*;
 
 	public static int solution(int[] arr) {
-
-		Arrays.sort(arr); // ASCENDING DEFAULT
-
+		Arrays.sort(arr); 
 		for (int i = 1; i < arr.length - 1; i++) {
 			if (((long) arr[i - 1] + (long) arr[i]) > (long) arr[i + 1])
 				return 1;
 		}
-
 		return 0;
-
 	}
 }
